ef85a08168fd99386e9d80d6d0133484e9825046
Devin1824 <devster18@gmail.com>
2022-12-27 12:10:03 -0800

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            word.ToLower();
+            //char[] charArray = word.ToCharArray();
+            //int fcount = 0;
+            // a string is already a char array so just go through the string index. Instead of charArray[] its just word[]
+            for (int i = 1; i <= word.Length / 2; i++)
+            {
+                if (word[i - 1] == word[word.Length - i])
+                {
+                    continue;
+                }
+                else
+                {
+                    //fcount += 1;
+                    return false;
+                }
+
+            }
+            return true;
+            //if (fcount > 0)
+            //{
+            //    return false;
+            //}
+            //else
+            //{
+            //    return true;
+            //}
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("house", false)]
+        [InlineData("cinegenic", true)]
+        [InlineData("deed", true)]
+        [InlineData("coding", false)]
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+            var instance = new WordSmith();
 
+            //Act
+            var actual = instance.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
